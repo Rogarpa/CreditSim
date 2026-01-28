c6df81c4c241c025bf4c75ac2be43dfb9038bcfd
Rogarpad <rodrigoestrategia2000@gmail.com>
2026-01-27 21:53:35 -0600

    [feat] Add cors middleware

--- app/main.py
+++ app/main.py
@@ -4,10 +4,26 @@ from app.models.dtos.AmortizationRequest import AmortizationRequest
 from fastapi import FastAPI, BackgroundTasks
 from app.connectors.SQLiteConnector import create_db_and_tables
 from app.services.LoanService import LoanService
-
+from fastapi.middleware.cors import CORSMiddleware
+from time import sleep
 create_db_and_tables()
 app = FastAPI(title="CreditSim")
 
+
+
+# Define the list of allowed origins (e.g., your frontend application's URL)
+origins = [
+        "*"
+
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/health")
 def health():
